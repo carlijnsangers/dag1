d0c092aefb5307e5d7a71a4135ae2cdf6f12ff9a
Carlijn Sangers <carlijnsangers@MacBook-Pro-van-Carlijn-2.local>
2023-09-05 16:20:29 +0200

    Aanpassingen

--- simple.py
+++ simple.py
@@ -1,12 +1,13 @@
 #commentaar
 
 print("hello world!")
+print("Dit is voor git")
 
 def funct1():
     print('Test')
 
 def funct2(tekst):
-    print('Ik print hier' + tekst)
+    print('Ik print 1234 hier' + tekst)
 
 funct1()
 funct2("Mijn tektst")
